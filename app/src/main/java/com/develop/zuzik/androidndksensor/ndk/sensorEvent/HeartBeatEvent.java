a429add1f75a3a558874fc291d1a2324468c7990
Yaroslav Zozulia <yaroslavzozulia@yzozulya-MacBook-Pro.local>
2019-02-07 16:19:26 +0200

    removed unused files

--- app/src/main/java/com/develop/zuzik/androidndksensor/ndk/sensorEvent/HeartBeatEvent.java
+++
@@ -1,9 +0,0 @@
-package com.develop.zuzik.androidndksensor.ndk.sensorEvent;
-
-/**
- * yaroslavzozulia
- * 2/7/19.
- */
-public class HeartBeatEvent implements SensorEvent {
-    //TODO:
-}
--- app/src/main/java/com/develop/zuzik/androidndksensor/ndk/sensorEvent/LowLatencyOffBodyDetectEvent.java
+++
@@ -1,9 +0,0 @@
-package com.develop.zuzik.androidndksensor.ndk.sensorEvent;
-
-/**
- * yaroslavzozulia
- * 2/7/19.
- */
-public class LowLatencyOffBodyDetectEvent implements SensorEvent {
-    //TODO:
-}
